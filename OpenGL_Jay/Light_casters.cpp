47443ff5766fbb7682477c2e48477128f419e785
wnwoghd22 <53481297+wnwoghd22@users.noreply.github.com>
2021-12-12 13:27:04 +0900

    light casters ~ in progress

--- OpenGL_Jay/Light_casters.cpp
+++ OpenGL_Jay/Light_casters.cpp
@@ -206,8 +206,11 @@ int main()
         //lightingShader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
         //lightingShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f); // darken diffuse light a bit
         lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
-        lightingShader.setVec3("light.position", lightPos);
+        //lightingShader.setVec3("light.position", lightPos);
         //lightingShader.setVec3("light.direction", lightPos);
+        lightingShader.setVec3("light.position", camera.Position);
+        lightingShader.setVec3("light.direction", camera.Front);
+        lightingShader.setFloat("light.cutOff", glm::cos(glm::radians(12.5f)));
         lightingShader.setFloat("light.constant", 1.0f);
         lightingShader.setFloat("light.linear", 0.09f);
         lightingShader.setFloat("light.quadratic", 0.032f);

70b7344cf051bd95c97c07d7e45496de85321857
wnwoghd22 <53481297+wnwoghd22@users.noreply.github.com>
2021-12-14 20:53:24 +0900

    depth testing ~ complete

--- OpenGL_Jay/shader.h
+++ OpenGL_Jay/shader.h
@@ -28,9 +28,9 @@ public:
     void setBool(const std::string& name, bool value) const;
     void setInt(const std::string& name, int value) const;
     void setFloat(const std::string& name, float value) const;
-    void setMat4(const std::string& name, glm::mat4 &value) const;
+    void setMat4(const std::string& name, const glm::mat4& value) const;
     void setVec3(const std::string& name, float x, float y, float z) const;
-    void setVec3(const std::string& name, glm::vec3 &value) const;
+    void setVec3(const std::string& name, const glm::vec3& value) const;
 };
 
 #endif
